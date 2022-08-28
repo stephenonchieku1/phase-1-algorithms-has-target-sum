c37de61a402a07c43daa8810f067d308419330a6
stephenonchieku1 <onchiekustephen901@gmail.com>
2022-08-29 01:25:04 +0300

    completed

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  if (array.length < 2) {
+    return false
+  }
+  for (let i= 0; i < array.length - 1; i++) {
+     for (let j= i+ 1; j < array.length; j++) {
+      let sum = array[i] + array[j];
+      if (sum === target) {
+        return true;
+      }
+    }
+  }
+  return false
 }
 
 /* 
